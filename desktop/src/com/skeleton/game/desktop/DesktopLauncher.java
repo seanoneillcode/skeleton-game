8f3cfabfad97745f0ce639bc2cd0a8fbb1fd67dc
sean oneill <sean.oneill@forgerock.com>
2016-11-30 13:57:11 +0000

    added wizard game

--- desktop/src/com/skeleton/game/desktop/DesktopLauncher.java
+++ desktop/src/com/skeleton/game/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package com.skeleton.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skeleton.game.CromwellGame;
 import com.skeleton.game.SkeletonGame;
+import com.skeleton.game.WizardGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SkeletonGame(), config);
+		config.width = 1024;
+		config.height = 768;
+		new LwjglApplication(new WizardGame(), config);
 	}
 }
